68eec0e5bb094282619e5d6c3e97594d73aa05b7
kokiSAT0 <kokisato0123@gmail.com>
2025-06-04 19:26:30 +0900

    Remove navigation, make single gamma screen

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaView } from 'react-native';
+import FlowRateConverter from './src/components/FlowRateConverter';
 
 // アプリのエントリーポイント
 export default function App() {
   return (
-    <>
-      {/* ナビゲーションコンポーネントを呼び出す */}
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* γ計算のみを行うシンプルな画面 */}
       <StatusBar style="auto" />
-      <AppNavigator />
-    </>
+      <FlowRateConverter />
+    </SafeAreaView>
   );
 }
